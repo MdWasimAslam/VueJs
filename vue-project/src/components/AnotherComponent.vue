d688643b36adc7a730a7c289c239b9ef57d98acb
Md Wasim Aslam <wasimaslam2897@gmail.com>
2024-10-24 00:44:20 +0530

    Update Component

---
+++ vue-project/src/components/AnotherComponent.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h1>Child Component</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste pariatur magnam voluptate quasi necessitatibus
+            temporibus est nemo inventore, consequuntur aperiam!</p>
+    </div>
+</template>
--- vue-project/src/components/HelloWorld.vue
+++ vue-project/src/components/HelloWorld.vue
@@ -1,44 +1,26 @@
+<!-- JavaScript -->
 <script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true,
-  },
-})
+import AnotherComponent from './AnotherComponent.vue';
 </script>
 
+
+<!-- HTML -->
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
-  </div>
+    <div>
+    <h1>Parent Component</h1>
+    <AnotherComponent />
+    </div>
 </template>
 
+
+
+
+<!-- CSS -->
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
+h1{
+    color: rgb(126, 126, 126);
+    font-size: 50px;
+    text-align: center;
+    margin-top: 50px;
 }
 </style>
